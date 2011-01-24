acaf70fb7fb09c2b94300101c2cbee1ba62ca1ef
Zach Denton <zacharydenton@gmail.com>
2011-01-24 10:08:39 -0500

    remove non-zeropadded directories

--- 16/sum-exp.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# 2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-# 
-# What is the sum of the digits of the number 2^(1000)?
-
-print sum(int(digit) for digit in str(2**1000))
