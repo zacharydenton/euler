9463710d69780c7663008c412d9abe6ab34ce1bf
Zach Denton <zacharydenton@gmail.com>
2011-03-22 16:50:20 -0400

    converted some questions to txt/html

--- 034/curious.clj
+++ 034/curious.clj
@@ -1,9 +1,4 @@
 #!/usr/bin/env clojure
-; 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
-;
-; Find the sum of all numbers which are equal to the sum of the factorial of their digits.
-;
-; Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 (defn factorial [n]
   (if (or (= n 1) (= n 0)) 
     1 
@@ -25,5 +20,4 @@
   (and (= (apply + (map factorial (digits number))) number)
        (> (count (digits number)) 1)))
 
-
-(time (println (reduce + (filter curious? (range 50000)))))
+(println (reduce + (filter curious? (range 50000))))
