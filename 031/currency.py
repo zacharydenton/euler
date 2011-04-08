c764bdb2351d6f3d60b13a13e305ee7f16bf88bf
Zach Denton <zacharydenton@gmail.com>
2011-04-08 16:41:13 -0400

    solved problem 31

---
+++ 031/currency.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+def ways_to_change(target, coins):
+    if target == 0 or len(coins) == 1:
+        return 1
+    else:
+        coins = sorted(coins)
+        largest = coins[-1]
+        uses = target / largest
+        total = 0
+        for i in range(uses + 1):
+            total += ways_to_change(target - largest * i, coins[:-1])
+        return total
+
+def main():
+    print ways_to_change(200, [1, 2, 5, 10, 20, 50, 100, 200])
+
+if __name__ == "__main__":
+    main()
