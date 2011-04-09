39dcfc3395eb9575fdd42b2f8c215c47432ab4fe
Zach Denton <zacharydenton@gmail.com>
2011-04-08 20:13:37 -0400

    solved #57

---
+++ 057/root-two.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import re
+import fractions
+
+def root_two(iterations):
+    d = fractions.Fraction(1/2)
+    for i in range(iterations):
+        d = fractions.Fraction(1/(2+d))
+        yield 1 + d
+
+def main():
+    print len([f for f in root_two(1000) if len(str(f.numerator)) > len(str(f.denominator))])
+
+if __name__ == "__main__":
+    main()
