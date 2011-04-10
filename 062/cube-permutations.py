32880f5bbde257176541936cc0d7378e60c6fab4
Zach Denton <zacharydenton@gmail.com>
2011-04-10 00:04:22 -0400

    solved #62

---
+++ 062/cube-permutations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from collections import defaultdict
+def cube(x): return x**3
+
+def main():
+    cubes = defaultdict(list)
+    for i in range(10000):
+        c = cube(i)
+        digits = ''.join(sorted([d for d in str(c)]))
+        cubes[digits].append(c)
+    print min([min(v) for k, v in cubes.items() if len(v) == 5])
+
+if __name__ == "__main__":
+    main()
