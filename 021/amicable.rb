7d74e2efd42b0319256297739addeafee260570a
Zach Denton <zacharydenton@gmail.com>
2011-08-03 19:50:44 -0400

    21 in ruby

---
+++ 021/amicable.rb
@@ -0,0 +1,19 @@
+require 'mathn' 
+
+class Integer 
+	def divisors
+		return [1] if self == 1
+		primes, powers = self.prime_division.transpose 
+		exponents = powers.map{|i| (0..i).to_a} 
+		divisors = exponents.shift.product(*exponents).map do |powers| 
+			primes.zip(powers).map{|prime, power| prime ** power}.inject(:*) 
+		end 
+		divisors.sort.take divisors.length - 1
+	end
+	
+	def amicable?(n=self.divisors.reduce(:+))
+		n != self && n.divisors.reduce(:+) == self
+	end
+end
+
+puts (1..10000).find_all { |n| n.amicable? }.reduce(:+)
