f51638f1ada185a35bc2feffc5b429b60cd7b710
Zach Denton <zacharydenton@gmail.com>
2011-08-03 13:41:23 -0400

    solved 12 with ruby

---
+++ 012/triangle-numbers.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'mathn' 
+
+class Integer 
+	def divisors
+		return [1] if self == 1
+		primes, powers = self.prime_division.transpose 
+		exponents = powers.map{|i| (0..i).to_a} 
+		divisors = exponents.shift.product(*exponents).map do |powers| 
+			primes.zip(powers).map{|prime, power| prime ** power}.inject(:*) 
+		end 
+		divisors.sort.map{|div| [div, self / div]} 
+	end
+end
+
+triangles = Enumerator.new do |yielder|
+	i = 1
+	loop do
+		yielder.yield i * (i + 1) / 2
+		i += 1
+	end
+end
+
+puts triangles.detect { |t| t.divisors.count > 500 }
