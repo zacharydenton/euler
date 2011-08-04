5a63103ec82a55b83c950e0987149bc91c923ae1
Zach Denton <zacharydenton@gmail.com>
2011-08-04 19:06:17 -0400

    30 in ruby

--- 030/fifth-powers.clj
+++ 030/fifth-powers.clj
@@ -12,9 +12,6 @@
   (map #(- (int %) 48) (str number))) 
 
 (defn can-be-written-as-sum-of-fifths? [number]
-  ; if the sum of the digits raised to
-  ; themselves is equal to the number 
-  ; itself, then it is a munchausen number.
   (and (not (= number 1)) (= (apply + (map #(** % 5) (digits number))) number)))
 
 (println (reduce + (filter can-be-written-as-sum-of-fifths? (range 500000))))
---
+++ 030/sum-of-fifths.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+puts (2..500000).select { |i|
+	i == i.to_s.each_char.map { |d| d.to_i**5 }.reduce(:+)
+}.reduce(:+)
