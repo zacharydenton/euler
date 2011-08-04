1124ed5d0817c6415c9a259aeebc0a0cfd5343c6
Zach Denton <zacharydenton@gmail.com>
2011-08-04 07:59:37 -0400

    22 in ruby

---
+++ 022/names.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+names = File.open('names.txt').read.scan(/\w+/).sort
+puts names.map { |name| 
+	word_score = name.each_byte.map { |c| c - 64 }.reduce(:+)
+	(names.index(name) + 1) * word_score
+}.reduce(:+)
