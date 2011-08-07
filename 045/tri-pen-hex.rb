130bc0cc1ed0484b01af025a613cdd60a4417cce
Zach Denton <zacharydenton@gmail.com>
2011-08-07 17:02:01 -0400

    45 in ruby

---
+++ 045/tri-pen-hex.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+require 'set'
+p = (1..100000).map { |n| n*(3*n-1)/2 }.to_set
+h = (1..100000).map { |n| n*(2*n-1) }.to_set
+puts (286..100000).map { |n| n*(n+1)/2 }.detect { |t|
+	p.include?(t) and h.include?(t)
+}
