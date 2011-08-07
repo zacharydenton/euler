6bcb58ee220604a04ef492f91e9ee250ea56af1c
Zach Denton <zacharydenton@gmail.com>
2011-08-07 04:50:42 -0400

    39 in ruby

--- 039/triangle-solve.py
+++ 039/triangle-solve.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
-# If p is the perimeter of a right angle triangle with 
-# integral length sides, {a,b,c}, there are exactly three 
-# solutions for p = 120.
-# 
-# {20,48,52}, {24,45,51}, {30,40,50}
-# 
-# For which value of p < 1000, is the number of solutions 
-# maximised?
 from collections import defaultdict
-
-def is_triple(a,b,c):
-    if (0 < a < b < c) and (a**2 + b**2 == c**2):
-        return True
-    return False
         
-def solve_triangle(perimeter, limit=None):
-    if limit is None:
-        limit = perimeter
-    for a in range(1, limit+1):
-        for b in range(a, limit+1):
-            for c in range(b, limit+1):
-                if a+b+c == perimeter:
-                    if is_triple(a,b,c):
-                        yield (a,b,c)
-
 def calculate_solutions(limit):
     res = defaultdict(list)
     for a in range(1, limit+1):
---
+++ 039/triangle-solve.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+solutions = {}
+(1..500).each do |a|
+	(a..500).each do |b|
+		(b..500).each do |c|
+			if a**2 + b**2 == c**2
+				solutions[a+b+c] ||= 0
+				solutions[a+b+c] += 1
+			end
+		end
+	end
+end
+puts solutions.max { |a,b| a[1] <=> b[1] }[0]
