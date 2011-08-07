21af0f6aed624af041be8d901af909fe5c6fe9d2
Zach Denton <zacharydenton@gmail.com>
2011-08-07 16:53:20 -0400

    44 in ruby

---
+++ 044/pentagonal-nums.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+require 'set'
+pentagonals = (1..3000).map { |n| n*(3*n-1)/2 }.to_set
+puts pentagonals.to_a.combination(2).select { |a,b|
+	pentagonals.include?(a+b) && pentagonals.include?((a-b).abs)
+}.map { |a,b| (a-b).abs }.min
