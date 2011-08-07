f338f6aef1fb98578855b80eaf0bee4ecdd56c76
Zach Denton <zacharydenton@gmail.com>
2011-08-06 21:34:24 -0400

    38 in ruby

---
+++ 038/pandigital-concatenation.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+p (1..9999).flat_map { |i|
+	(1..9).map { |j|
+		(1..j).map { |k|
+			i * k
+		}.reduce('') { |s,v| s + v.to_s }
+	}
+}.select { |s|
+	s.length == 9 && s.split('').sort.join('') == '123456789'
+}.map { |s| s.to_i }.max
+
