9ab0f3f22595181392ebc52b63318e8f549fb44a
Zach Denton <zacharydenton@gmail.com>
2011-08-08 22:51:36 -0400

    50 in ruby

--- 050/consecutive-prime-sum.py
+++ 050/consecutive-prime-sum.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
-# The prime 41, can be written as the sum of six consecutive primes:
-# 
-#   41 = 2 + 3 + 5 + 7 + 11 + 13
-# 
-# This is the longest sum of consecutive primes that adds to a prime 
-# below one-hundred.
-# 
-# The longest sum of consecutive primes below one-thousand that adds 
-# to a prime, contains 21 terms, and is equal to 953.
-# 
-# Which prime, below one-million, can be written as the sum of the 
-# most consecutive primes?
+
 def euler(n):
     # Create a candidate list within which non-primes will
     # marked as None, noting that only candidates below
@@ -29,21 +18,6 @@ def euler(n):
  
     # Filter out non-primes and return the list.
     return [i for i in candidates[2:] if i]
-
-def largest_consecutive_seq(primes):
-    largest = []
-    for prime in primes:
-        total = 0
-        for i, a in enumerate(primes[:primes.index(prime)]):
-            j = 0
-            while total < prime:
-                total += primes[i+j]
-                j += 1
-            if total == prime:
-                seq = primes[i:i+j]
-                if len(seq) > len(largest):
-                    largest = seq
-    return largest
 
 def main():
     primes = euler(1000000)
