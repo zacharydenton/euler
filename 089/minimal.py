0dcc83ee0a3f41c97ab04d91bc3aed7c5740a354
Zach Denton <zacharydenton@gmail.com>
2012-01-23 01:31:47 -0500

    tidied 89

--- 089/minimal.py
+++ 089/minimal.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
 import os
-import operator
-
-numerals = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000,
-}
 
 def subtractive(roman):
     result = roman
