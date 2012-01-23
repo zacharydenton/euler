8251e1f297d56edad533fd5e322d7eab09094092
Zach Denton <zacharydenton@gmail.com>
2012-01-23 00:25:53 -0500

    fixed 99

--- 099/largest-exponent.py
+++ 099/largest-exponent.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+import os
 from math import log10
 largest = [0, 0]
-for i, line in enumerate(open('base_exp.txt')):
+for i, line in enumerate(open(os.path.join(os.path.dirname(__file__), 'base_exp.txt'))):
     a, x = map(int, line.split(','))
     if x * log10(a) > largest[0]:
         largest = [x * log10(a), i+1]
