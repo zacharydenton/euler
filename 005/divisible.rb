b82d7ea8503c8303c398196f96c73b73ee34f293
Zach Denton <zacharydenton@gmail.com>
2012-03-08 12:59:16 -0500

    made ruby solution for #5 more idiomatic

--- 005/divisible.rb
+++ 005/divisible.rb
@@ -1,31 +1,24 @@
 #!/usr/bin/env ruby
 
 class Numeric
-    def divisible_to?(x)
-        (1..x).each do |i|
-            if self % i != 0 
-                return false
-            end
-        end
-        return true
-    end
+	def divisible_to?(x)
+		self > 0 and (1..x).all? { |i| self % i == 0 }
+	end
 end
 
 def divisible_to(x)
-    if x < 1
-        return false
-    elsif x == 1
-        return 1
-    else
-        step = divisible_to(x-1)
-        number = 0
-        found = false
-        while not found
-            number += step
-            found = number.divisible_to? x
-        end
-        return number
-    end
+	if x < 1
+		return false
+	elsif x == 1
+		return 1
+	else
+		n = 0
+		step = divisible_to(x-1)
+		until n.divisible_to? x
+			n += step
+		end
+		return n
+	end
 end
 
 puts divisible_to(20)
