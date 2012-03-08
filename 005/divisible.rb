5feacf35e8140d70fca07575508c3caef86ff9d0
Zach Denton <zacharydenton@gmail.com>
2012-03-08 13:06:32 -0500

    nicer ruby indentation

--- 005/divisible.rb
+++ 005/divisible.rb
@@ -1,24 +1,24 @@
 #!/usr/bin/env ruby
 
 class Numeric
-	def divisible_to?(x)
-		self > 0 and (1..x).all? { |i| self % i == 0 }
-	end
+  def divisible_to?(x)
+    self > 0 and (1..x).all? { |i| self % i == 0 }
+  end
 end
 
 def divisible_to(x)
-	if x < 1
-		return false
-	elsif x == 1
-		return 1
-	else
-		n = 0
-		step = divisible_to(x-1)
-		until n.divisible_to? x
-			n += step
-		end
-		return n
-	end
+  if x < 1
+    return false
+  elsif x == 1
+    return 1
+  else
+    n = 0
+    step = divisible_to(x-1)
+    until n.divisible_to? x
+      n += step
+    end
+    return n
+  end
 end
 
 puts divisible_to(20)
