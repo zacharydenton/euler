7c8fb6aa3574157c4b28012129d682f390366bbd
Zach Denton <zacharydenton@gmail.com>
2012-04-30 15:28:57 -0400

    convert python #112 to pure functional/itertools

--- 112/bouncy.py
+++ 112/bouncy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from itertools import *
-from fractions import Fraction
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
@@ -20,13 +19,15 @@ def is_decreasing(n):
 def is_bouncy(n):
     return not is_increasing(n) and not is_decreasing(n)
 
+def running_total(iterable):
+    total = 0
+    for element in iterable:
+        total += element
+        yield total
+
 def main():
-    bouncy = 0.0
-    for n in count(1):
-        if is_bouncy(n):
-            bouncy += 1
-        if bouncy / n == 0.99:
-            print n
-            break
+    nums = count(1)
+    bouncy = running_total(imap(lambda n: float(is_bouncy(n)), count(1)))
+    print (n for n, b in izip(nums, bouncy) if b / n == 0.99).next()
 
 if __name__ == "__main__": main()
