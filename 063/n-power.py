ff1a1c6be31321b39f1b8ac483e0c0608c582f72
Zach Denton <zacharydenton@gmail.com>
2012-04-30 09:39:52 -0400

    make python solution for 63 more concise

--- 063/n-power.py
+++ 063/n-power.py
@@ -1,23 +1,2 @@
 #!/usr/bin/env python
-from itertools import *
-
-def get_digits(n):
-    return str(n)
-
-def is_npower(n):
-    digits = str(n)
-    ndigits = len(digits)
-    for i in range(2,10):
-        if i ** ndigits == n:
-            return True
-    return False
-
-def main():
-    count = 0
-    for i, x in product(range(1,100), range(1,100)):
-        if len(get_digits(i ** x)) == x:
-            count += 1
-    print count
-
-if __name__ == "__main__":
-    main()
+print sum(1 for i in range(1,100) for x in range(1,100) if len(str(i**x)) == x)
