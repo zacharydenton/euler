e4bd3053da1894a131faa84e3155d86a8dbd9b3a
Zach Denton <zacharydenton@gmail.com>
2012-04-30 14:22:38 -0400

    solve #71 in python

---
+++ 071/prop-frac.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from fractions import Fraction
+
+def main():
+    three_sevenths = 3.0 / 7
+    closest = Fraction(three_sevenths).limit_denominator(1000000)
+    while closest == Fraction(3, 7):
+        three_sevenths -= 1e-6
+        closest = Fraction(three_sevenths).limit_denominator(1000000)
+    print closest.numerator
+
+if __name__ == "__main__": main()
+
