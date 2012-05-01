c969556099d439a939ff75661876067c6bde23e2
Zach Denton <zacharydenton@gmail.com>
2012-05-01 11:20:06 -0400

    solve #206 in python

---
+++ 206/concealed.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def is_concealed(n):
+    digits = str(n)
+    template = "1_2_3_4_5_6_7_8_9_0"
+
+    if len(digits) != len(template):
+        return False
+
+    return all(digits[i] == template[i] for i in range(0, len(digits), 2))
+
+def main():
+    print (n for n in xrange(1360000000, 1390000000, 10)
+        if (n % 100 == 30 or n % 100 == 70) and is_concealed(n*n)).next()
+
+if __name__ == "__main__": main()
---
+++ 206/question.markdown
@@ -0,0 +1,2 @@
+Find the unique positive integer whose square has the form 1\_2\_3\_4\_5\_6\_7\_8\_9\_0,
+where each "\_" is a single digit.
