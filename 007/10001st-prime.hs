b6f41e38346355409470f66823bc2f356e82cdb1
Zach Denton <z@chdenton.com>
2014-05-14 21:08:48 -0400

    Solve #7 in Haskell.

---
+++ 007/10001st-prime.hs
@@ -0,0 +1,7 @@
+primes :: [Integer]
+primes = 2 : sieve primes [3,5..] where
+    sieve (p:ps) xs = h ++ sieve ps [x | x <- t, rem x p /= 0]
+                      where (h, t) = span (< p*p) xs
+
+main :: IO ()
+main = print $ primes !! 10000
