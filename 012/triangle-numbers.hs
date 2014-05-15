984248523a44915ec9a0235ab3f55289bf61ca0c
Zach Denton <z@chdenton.com>
2014-05-14 23:09:54 -0400

    Solve #12 in Haskell.

---
+++ 012/triangle-numbers.hs
@@ -0,0 +1,31 @@
+import Data.List (group)
+
+triangles :: [Int]
+triangles = scanl1 (+) [1..]
+
+primes :: [Int]
+primes = sieve [2..] where
+    sieve [] = []
+    sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p > 0]
+
+factorize :: Int -> [Int]
+factorize n = primeFactors n primes where
+    primeFactors 1 _ = []
+    primeFactors _ [] = []
+    primeFactors m (p:ps) | m < p * p = [m]
+                          | r == 0 = p : primeFactors q (p:ps)
+                          | otherwise = primeFactors m ps
+                          where (q, r) = quotRem m p
+
+primePowers :: Int -> [(Int, Int)]
+primePowers n = [(head x, length x) | x <- group $ factorize n]
+
+divisors :: Int -> [Int]
+divisors n = map product $ sequence
+    [take (k+1) $ iterate (p*) 1 | (p, k) <- primePowers n]
+
+numDivisors :: Int ->  Int
+numDivisors n = product [k + 1 | (_, k) <- primePowers n]
+
+main :: IO ()
+main = print $ head $ dropWhile (\n -> numDivisors n <= 500) triangles
