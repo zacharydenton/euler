4c3bbb4718b23918e0b5084363a3da9830fe4d7f
Zach Denton <z@chdenton.com>
2014-05-14 21:53:08 -0400

    Solve #8 in Haskell.

---
+++ 008/great-prod.hs
@@ -0,0 +1,16 @@
+parse :: String -> [Int]
+parse = map (read . return) . concat . lines
+
+chunks :: Int -> [a] -> [[a]]
+chunks n l
+    | length chunk < n = []
+    | otherwise = chunk : chunks n (tail l)
+    where chunk = take n l
+
+largestProduct :: [[Int]] -> Int
+largestProduct = maximum . map product
+
+main :: IO ()
+main = do
+        str <- readFile "/home/zach/code/euler/008/numbers.txt"
+        print $ largestProduct $ chunks 5 $ parse str
