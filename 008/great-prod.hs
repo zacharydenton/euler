7a897dae0fc5691bd999dfe0a5d5d98543e96843
Zach Denton <z@chdenton.com>
2014-05-14 22:03:38 -0400

    Solve #10 in Haskell.

--- 008/great-prod.hs
+++ 008/great-prod.hs
@@ -2,10 +2,9 @@ parse :: String -> [Int]
 parse = map (read . return) . concat . lines
 
 chunks :: Int -> [a] -> [[a]]
-chunks n l
-    | length chunk < n = []
-    | otherwise = chunk : chunks n (tail l)
-    where chunk = take n l
+chunks n l | length chunk < n = []
+           | otherwise = chunk : chunks n (tail l)
+           where chunk = take n l
 
 largestProduct :: [[Int]] -> Int
 largestProduct = maximum . map product
