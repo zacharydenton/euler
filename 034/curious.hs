2953d6ece266fe1e27bff741b2c0956bfc85b78d
Zach Denton <z@chdenton.com>
2014-05-18 17:10:55 -0400

    Solve #34 in Haskell.

---
+++ 034/curious.hs
@@ -0,0 +1,14 @@
+factorial :: Int -> Int
+factorial n = product [1..n]
+
+digits :: Int -> [Int]
+digits 0 = []
+digits n = r : digits q
+    where (q, r) = quotRem n 10
+
+
+curious :: Int -> Bool
+curious n = n == sum (map factorial (digits n))
+
+main :: IO ()
+main = print $ sum $ filter curious [10..50000]
