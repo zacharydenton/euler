7e1f049ac3f2e0987b33be5070af1e3761c8ca22
Zach Denton <z@chdenton.com>
2014-05-18 00:00:52 -0400

    Delete zeropad.py

--- zeropad.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import os
-euler_dir = os.path.expanduser('~/bin/euler')
-for directory in os.listdir(euler_dir):
-    if os.path.isdir(directory) and directory.isdigit():
-        os.rename(os.path.join(euler_dir, directory), os.path.join(euler_dir, "%03i" % int(directory)))
