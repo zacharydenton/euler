24422972aa6b090b84990135c22194a122f0eda7
Zach Denton <z@chdenton.com>
2014-05-20 00:11:26 -0400

    Solve #60 in Haskell.

--- 058/spiral-primes.hs
+++ 058/spiral-primes.hs
@@ -3,7 +3,7 @@ isPrime n | n < 1 = False
           | otherwise = not $ or [n `rem` x == 0 | x <- [2..floor $ sqrt $ fromIntegral n]]
 
 primeCorners :: Int -> Int
-primeCorners n = sum [1 | x <- [0..3], isPrime $ n^2 - x*(n - 1)]
+primeCorners n = sum [1 | x <- [1..3], isPrime $ n^2 - x*(n - 1)]
 
 expansion :: [(Int, Int)]
 expansion = scanl (\(p, t) x -> (p + primeCorners x, t + 4)) (0, 1) [3,5..]
---
+++ 060/prime-pairs.hs
@@ -0,0 +1,30 @@
+primes :: [Int]
+primes = 2 : sieve primes [3,5..] where
+    sieve (p:ps) xs = h ++ sieve ps [x | x <- t, rem x p /= 0]
+                      where (h, t) = span (< p*p) xs
+
+isPrime :: Int -> Bool
+isPrime n | n < 1 = False
+          | otherwise = not $ or [n `rem` x == 0 | x <- [2..floor $ sqrt $ fromIntegral n]]
+
+allPrime :: [Int] -> Bool
+allPrime ps = all isPrime [read ((show a) ++ (show b)) | a <- ps, b <- ps, a /= b]
+
+filterPairs :: Int -> [Int] -> [Int]
+filterPairs p = filter test where
+    test x = isPrime (read $ show x ++ show p) && isPrime (read $ show p ++ show x)
+
+candidates :: [[Int]]
+candidates = filter allPrime [[a, b, c, d, e] | a <- primes',
+                                                let bs = filterPairs a $ dropWhile (<= a) primes',
+                                                b <- bs,
+                                                let cs = filterPairs b $ dropWhile (<= b) bs,
+                                                c <- cs,
+                                                let ds = filterPairs c $ dropWhile (<= c) cs,
+                                                d <- ds,
+                                                let es = filterPairs c $ dropWhile (<= d) ds,
+                                                e <- es]
+    where primes' = takeWhile (< 10000) primes
+
+main :: IO ()
+main = print $ sum $ head candidates
