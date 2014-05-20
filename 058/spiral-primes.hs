8f1481c316314db106cc8f14f6f192bdc7af43d1
Zach Denton <z@chdenton.com>
2014-05-19 23:12:53 -0400

    Solve #58 in Haskell.

---
+++ 058/spiral-primes.hs
@@ -0,0 +1,12 @@
+isPrime :: Int -> Bool
+isPrime n | n < 1 = False
+          | otherwise = not $ or [n `rem` x == 0 | x <- [2..floor $ sqrt $ fromIntegral n]]
+
+primeCorners :: Int -> Int
+primeCorners n = sum [1 | x <- [0..3], isPrime $ n^2 - x*(n - 1)]
+
+expansion :: [(Int, Int)]
+expansion = scanl (\(p, t) x -> (p + primeCorners x, t + 4)) (0, 1) [3,5..]
+
+main :: IO ()
+main = print $ 3 + 2 * (length $ takeWhile id $ tail [10*p >= t | (p, t) <- expansion])
