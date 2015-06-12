297e48da1d9856ad730c3b12b76aea4cc87e2511
Zach Denton <z@chdenton.com>
2015-06-12 14:07:52 -0400

    Solve #4 in Go.

---
+++ 004/palindrome.go
@@ -0,0 +1,25 @@
+package main
+
+func IsPalindrome(number int) bool {
+	n := number
+	reversed, digit := 0, 0
+	for n > 0 {
+		digit = n % 10
+		reversed = reversed*10 + digit
+		n /= 10
+	}
+	return number == reversed
+}
+
+func main() {
+	product, max := 0, 0
+	for a := 100; a < 1000; a++ {
+		for b := a; b < 1000; b++ {
+			product = a * b
+			if product > max && IsPalindrome(product) {
+				max = product
+			}
+		}
+	}
+	println(max)
+}
