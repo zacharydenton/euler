d33b86b469c4d52992faf3efa466b9efc229a0f3
Zach Denton <z@chdenton.com>
2018-03-07 11:22:05 -0800

    Add rust solution for #11.

---
+++ 011/grid.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let grid: Vec<u64> = include_str!("grid.txt")
+        .split(|c| c == ' ' || c == '\n')
+        .filter_map(|n| n.parse().ok())
+        .collect();
+    let grid_length = grid.len() as isize;
+    let intervals: [isize; 4] = [1, 20, 21, -19];
+    let max_product: u64 = grid.iter()
+        .enumerate()
+        .flat_map(|(i, _)| {
+            intervals
+                .iter()
+                .filter_map(|interval| {
+                    let last = (i as isize) + 3 * interval;
+                    if last >= 0 && last < grid_length {
+                        let product = (0..4)
+                            .map(|x| grid[((i as isize) + x * interval) as usize])
+                            .product();
+                        Some(product)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        })
+        .max()
+        .unwrap();
+    println!("{}", max_product);
+}
