20d546b9620650a4b1f402df4b07c68b7c19958c
Zach Denton <z@chdenton.com>
2018-03-08 05:47:15 -0800

    Add rust solution for #14.

---
+++ 014/collatz.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut collatz: Vec<usize> = vec![0; 1000000];
+    collatz[1] = 1;
+    let max = (2..collatz.len())
+        .max_by_key(|&i| {
+            let mut j: usize = i;
+            let mut len = 0;
+            loop {
+                if j < 1000000 && collatz[j] != 0 {
+                    break;
+                }
+                len += 1;
+                if j % 2 == 0 {
+                    j /= 2;
+                } else {
+                    j = 3 * j + 1;
+                }
+            }
+            len += collatz[j];
+            collatz[i] = len;
+            len
+        })
+        .unwrap();
+    println!("{}", max);
+}
