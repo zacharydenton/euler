2113e0bbad9a692bab4b95a30e43f73e3af44a03
Zach Denton <z@chdenton.com>
2018-03-09 17:45:28 -0800

    Add rust solution for #25.

---
+++ 025/fibonacci.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut a = vec![0];
+    let mut b = vec![1];
+    let mut n = 1;
+    while b.len() < 1000 {
+        let tmp = b.clone();
+        let mut carry = 0;
+        for i in 0..b.len() {
+            if i >= a.len() {
+                a.push(0);
+            }
+            let mut digit = a[i];
+            digit = b[i] + digit + carry;
+            carry = digit / 10;
+            a[i] = digit % 10;
+        }
+        if (carry > 0) {
+            a.push(carry);
+        }
+        b = a;
+        a = tmp;
+        n += 1;
+    }
+    println!("{}", n);
+}
